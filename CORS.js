842de086516bf3b0ba7a0060a09fce924c6aaa82
Barney <130295277+JamesHickers@users.noreply.github.com>
2025-06-04 15:18:46 -0400

    Update CORS.js
    Signed-off-by: Barney <130295277+JamesHickers@users.noreply.github.com>

--- CORS.js
+++ CORS.js
@@ -1,7 +1,14 @@
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); 
-  next();
-});
+// CORS.js
+(async () => {
+  try {
+    const res = await fetch("https://corsproxy.io/?https://catbox.moe/user/api.php", {
+      method: "POST",
+      body: new FormData(), // you can modify this
+    });
+
+    const data = await res.text(); // or .json() if you expect JSON
+    console.log("It actually worked?", data);
+  } catch (err) {
+    console.error("CORS slapped you:", err);
+  }
+})();
